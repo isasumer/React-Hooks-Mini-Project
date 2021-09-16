2a162456a850c8d24e9b35c8497740dbf8221767
Isa SUMER <sumerisa5@gmail.com>
2021-09-16 23:32:34 +0300

    list added

---
+++ hooks/src/components/List.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ListItem from "./ListItem";
+
+const List = ({ students, add }) => {
+  console.log("Rendering:List Components");
+
+  return (
+    <div className="list">
+      {students?.map((student) => {
+        return <ListItem key={student.id} student={student} add={add} />;
+      })}
+    </div>
+  );
+};
+
+export default List;

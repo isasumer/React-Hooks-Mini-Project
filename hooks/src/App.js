34ba9b633b0bacd815337ee6f6f725e34c4725eb
Isa SUMER <sumerisa5@gmail.com>
2021-09-16 23:31:48 +0300

    app js upgraded

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, useEffect, useMemo, useCallback} from "react";
+import Header from "./components/Header";
+import List from "./components/List";
+import fs from "./assets/fs_logo.png";
+import aws from "./assets/aws_logo.png";
+import cw from "./assets/cw.png";
 
-function App() {
+
+const App = () => {
+  const [counter, setCounter] = useState (0);
+  const [img, setImg] = useState();
   return (
     <div className="App">
- isa SUMER
+      <Header img={img}/>
+      <p>Counter: {counter}</p>
+      <button onClick ={()=> setCounter(counter+1)}>Increase</button>
+      <button onClick ={()=> setImg(fs)}>FullStack</button>
+      <button onClick ={()=> setImg(aws)}>AWS</button>
+      <button onClick ={()=> setImg("")}>Reset</button>
     </div>
-  );
+  )
 }
-
 export default App;
